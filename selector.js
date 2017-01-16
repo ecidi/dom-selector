74e076ad3b6fc8acf272e065bab0cc69ec2185e3
xueying <catcxj@163.com>
2017-01-16 16:44:58 +0800

    添加 haschlass \ children 实现。

--- selector.js
+++ selector.js
@@ -6,33 +6,53 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import _ from 'lodash';
+
+// 判断节点是否拥有 className
+function hasClass(el, className){
+	if (el.classList) {
+		return el.classList.contains(className);
+	}
+	else {
+		return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
+	}
+}
+
+// 选择靠自己最近的且符合要求的父节点
 function closest(el, className){
-    let findParent = false;
-    let parent = el.parentNode;
-    while(!findParent){
-        // 到 body 终止查询
-        if (parent.nodeName === 'BODY') {
-            el = undefined;
-            break;
-        }
-        // 判断节点是否拥有 className
-        if (parent.classList) {
-            findParent = parent.classList.contains(className);
-        }
-        else {
-            findParent = new RegExp('(^| )' + className + '( |$)', 'gi').test(parent.className);
-        }
-        // 找到元素则保留值，没有则继续递归
-        if (findParent) {
-            el = parent;
-        } else {
-            parent = parent.parentNode;
-        }
-    }
-    return el;
+	let findParent = false;
+	let parent = el.parentNode;
+	while(!findParent){
+		// 到 body 终止查询
+		if (parent.nodeName === 'BODY') {
+			el = undefined;
+			break;
+		}
+		findParent = hasClass(parent, className);
+		// 找到元素则保留值，没有则继续递归
+		if (findParent) {
+			el = parent;
+		} else {
+			parent = parent.parentNode;
+		}
+	}
+	return el;
+}
+
+// 选择符合要求的直接子节点
+function children(el, className){
+	const list = [];
+	_.forEach(el.children, function(child){
+		if(hasClass(child, className)){
+			list.push(child);
+		};
+	});
+	return list;
 }
 
 // 对外接口
 export default {
-    closest
+	hasClass,
+	closest,
+	children
 };
